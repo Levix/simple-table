8f23279aa9ac7d72475fc6995cb0e188d76f6841
abu <838270547@qq.com>
2022-05-10 21:40:30 +0800

    feat: table实现
    改动：
    
    新增table+pagination组件
    
    评论修改

--- bsq/src/App.vue
+++ bsq/src/App.vue
@@ -1,11 +1,79 @@
 <script setup lang="ts">
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import SimpleTable from "./components/SimpleTable";
+import { reactive } from '@vue/reactivity'
+import SimpleTable from './components/SimpleTable'
+
+const tableOption = reactive({
+
+  // 排序是否是支持多个
+  // isMultiSort: true,
+
+  // 是否是本地排序：当前界面排序，非接口排序
+  isLocalSort: true,
+
+  // 列配置
+  columns: [
+    {
+      title: 'ID',
+      dataKey: 'id',
+      sortable: true
+    },
+    {
+      title: '姓名',
+      dataKey: 'name'
+    },
+    {
+      title: '年龄',
+      dataKey: 'age',
+      sortable: true
+    },
+    {
+      title: '爱好',
+      dataKey: 'hobby'
+    }
+  ],
+
+  // 数据源
+  data: [
+    {
+      id: '1111',
+      name: '张三',
+      age: '18',
+      hobby: '打篮球'
+    },
+    {
+      id: '3333',
+      name: '王五',
+      age: '26',
+      hobby: '打羽毛球'
+    },
+    {
+      id: '2222',
+      name: '李四',
+      age: '20',
+      hobby: '踢足球'
+    }
+  ]
+})
+
+const hanleSort = sortParams => {
+
+  // 这里处理远程排序参数下发请求
+  window.console.log('sortParams: ', sortParams)
+}
+
+const updateCurPage = curPage => {
+
+  // 这里处理远程当前页参数下发请求
+  window.console.log('curPage: ', curPage)
+}
 </script>
 
 <template>
-  <SimpleTable />
+  <simple-table :option="tableOption" 
+                @hanle-sort="hanleSort"
+                @update-cur-page="updateCurPage" />
 </template>
 
 <style>
--- bsq/src/components/SimpleTable.tsx
+++ bsq/src/components/SimpleTable.tsx
@@ -1,25 +1,75 @@
-import { defineComponent } from "vue";
+import { defineComponent, reactive, watch } from "vue";
 import { type TableProps, tableProps } from "./types";
+import { cloneDeep } from 'lodash';
+import Pagination from "./Pagination";
+import useSortable from "../hooks/useSortable";
+import './index.css'
 
 export default defineComponent({
-  name: "SimpleTable",
+  name: 'SimpleTable',
+  components: {
+    Pagination
+  },
   props: tableProps,
-  setup(props: TableProps, { attrs, emit, slots }) {
-    return () => {
-      return (
+  setup(props: TableProps, { emit }) {
+
+    let list = reactive(cloneDeep(props.option.data))
+
+    let { SORT_PARAMS_MAP, keyClickMap, sortableData, handleSort } = useSortable()
+
+    watch(sortableData, () => {
+      list = sortableData.value
+    })
+
+    watch(keyClickMap, () => {
+      let sortParams = Object.keys(keyClickMap.value).reduce((prev, key) => {
+        prev[key] = SORT_PARAMS_MAP[keyClickMap.value[key]]
+        return prev;
+      }, {})
+      emit('hanle-sort', sortParams)
+    }, {
+      deep: true
+    })
+
+    return () => (
+      <div class='table-wrap'>
         <table>
           <thead>
             <th>
-              <td></td>
+              {
+                props.option.columns.map((item: { title: string; sortable?: boolean; dataKey: string }) => (
+                  <td key={item.dataKey} class='table-th-td'>
+                    {item.title}
+                    {
+                      item.sortable &&
+                      <span
+                        class={`table-th-td__sort-btn status--${keyClickMap.value?.[item?.dataKey]}`}
+                        onClick={() => handleSort(props.option.data, item.dataKey, props.option.isMultiSort, props.option.isLocalSort)}>{'>'}</span>
+                    }
+                  </td>
+                ))
+              }
             </th>
           </thead>
           <tbody>
-            <tr>
-              <td>hello table</td>
-            </tr>
+            {
+              list.map((item: any) => (
+                <tr key={item.id}>
+                  {
+                    props.option.columns.map((configItem: { dataKey: string }) => (
+                      <td key={configItem.dataKey}>{item[configItem.dataKey]}</td>
+                    ))
+                  }
+                </tr>
+              ))
+            }
           </tbody>
         </table>
-      );
-    };
+
+        <pagination total={100} limit={10} onUpdateCurPage={(value: number) => {
+          emit('update-cur-page', value)
+        }} />
+      </div>
+    );
   },
 });
